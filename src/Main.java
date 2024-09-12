796994de84173ff42b3c4d7f99b0cb32b09e5ced
topeieotpiert <fdghfghfgh@email.com>
2024-09-12 15:14:19 +0300

    three marks

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         stringManager object = new stringManager("hello");
-        System.out.println(object.getNumberString());
+        object.printString();
     }
 }
